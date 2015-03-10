ba80920b165a155a29ff9b9773f60107e42c6412
suzuki525 <suzuki525@gmail.com>
2015-03-10 15:19:21 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/suzuki525/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  384,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:03:41.344Z",
+  "2015-03-10T07:03:41.342Z",
+  "2015-03-10T07:03:41.486Z",
+  "2015-03-10T07:04:11.466Z",
+  "2015-03-10T07:03:41.418Z",
+  "2015-03-10T07:03:41.421Z",
+  "2015-03-10T07:03:41.307Z",
+  "2015-03-10T07:03:41.291Z",
+  "2015-03-10T07:03:57.770Z"
+ ],
+ "date": "2015-03-10T07:04:12.436Z"
+})
